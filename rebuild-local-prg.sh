4a6bcf2d83253e5bf53ceb20a69f1b7418313c9d
Honza Piskvor Martinec <github@piskvor.org>
2017-10-27 17:08:02 +0200

    add mounts + prepare dirs

--- rebuild-local-prg.sh
+++ rebuild-local-prg.sh
@@ -7,8 +7,11 @@ HERE=$(dirname $(realpath $0))
 FULL=0
 NOCACHE=0
 DOCKER_ARGS=""
-CACHE_BUSTER="$(date)"
-DOCKER_VOLUMES="" #-v ${HERE}/mounts/html:/html -v ${HERE}/mounts/osmtracker:/osmtracker
+CACHE_BUSTER=""
+#CACHE_BUSTER="$(date)"
+DOCKER_VOLUMES=""
+DOCKER_VOLUMES="-v ${HERE}/mounts/osmtracker:/osmtracker"
+#DOCKER_VOLUMES="-v ${HERE}/mounts/html:/html -v ${HERE}/mounts/osmtracker:/osmtracker"
 
 if [ "${1:-}" = "--full" ]; then
 	FULL=1
@@ -47,7 +50,22 @@ sudo docker build ${DOCKER_ARGS} \
 	--build-arg REGIONS_URL_PREFIX="https://maps.piskvor.org/regions" \
 	-t osm-analytic-tracker \
 	.
+
 docker stop osmat-container && docker rm osmat-container || true
+
+if [ "$DOCKER_VOLUMES" != "" ]; then
+	mkdir -p ${HERE}/mounts/html/dynamic
+	cp -r ${HERE}/html ${HERE}/mounts/
+	cp  ${HERE}/html/starting.html ${HERE}/mounts/html/index.html
+	mkdir -p ${HERE}/mounts/osmtracker/osm-analytic-tracker
+	cp ${HERE}/*.py ${HERE}/logging.conf ${HERE}/mounts/osmtracker/osm-analytic-tracker
+	cp -r ${HERE}/osm ${HERE}/mounts/osmtracker/osm-analytic-tracker/
+#	cp -r ${HERE}/templates ${HERE}/mounts/osmtracker/
+	cp -r ${HERE}/templates ${HERE}/mounts/osmtracker/osm-analytic-tracker/
+	cp ${HERE}/docker/supervisord.conf ${HERE}/mounts/osmtracker/
+#	exit;
+fi
+
 docker run -d --restart=always --name=osmat-container -p 80:80 ${DOCKER_VOLUMES} osm-analytic-tracker:latest
 sleep 5
 docker ps
